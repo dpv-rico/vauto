62d0597e6b7834397ff19acac8111cd006b39172
Enrico Müller <e.mueller@dpv-analytics.com>
2024-10-23 23:19:15 +0200

    add project

---
+++ TestApp/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+// fix
+// blub 4
+// blub 5
+// sjsklfsd
---
+++ TestLib/Class1.cs
@@ -0,0 +1,17 @@
+﻿namespace TestLib;
+
+public class Class1
+{
+    // feat 1
+    // feat 2
+    // feat 3
+    // feat 4
+    // blub 4
+    // fix 1
+    // blub 7
+    // feat newdfdf
+    // feat 2
+    // feat 3
+    // feat 4
+    // feat877778888dfsdfd
+}
