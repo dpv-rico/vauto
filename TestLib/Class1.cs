57836186f2200c002413a5b712e62354c6242de5
Enrico Müller <e.mueller@dpv-analytics.com>
2024-12-02 11:54:09 +0100

    feat: 1

--- TestLib/Class1.cs
+++ TestLib/Class1.cs
@@ -3,15 +3,4 @@
 public class Class1
 {
     // feat 1
-    // feat 2
-    // feat 3
-    // feat 4
-    // blub 4
-    // fix 1
-    // blub 7
-    // feat newdfdf
-    // feat 2
-    // feat 3
-    // feat 4
-    // feat877778888dfsdfd
 }
