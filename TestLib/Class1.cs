116c06f72af0b106ed9079b142227b21f736eeb7
Enrico Müller <e.mueller@dpv-analytics.com>
2024-12-03 16:24:11 +0100

    fix: today

--- TestLib/Class1.cs
+++ TestLib/Class1.cs
@@ -2,11 +2,14 @@
 
 public class Class1
 {
+    // today
     // feat 1
     // feat 3
     // conflict 999
+    // today
     // fix 10
     // fix 2020
     // confi 666
     // conflict 999
+    // today
 }
