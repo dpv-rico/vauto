92da6e6e3fb1dfb585a7ede3c5e3bfe33df533b3
Enrico Müller <e.mueller@dpv-analytics.com>
2026-01-06 22:57:01 +0100

    feat: #1

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,10 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-
 // feat #1
-// feat #2
-// fix #1
-// fix #2
-// feat #3
-// fix #3
--- TestLib/Class1.cs
+++ TestLib/Class1.cs
@@ -2,13 +2,5 @@
 
 public class Class1
 {
-    // feat 1
-    // feat 3
-    // conflict 999
-    // fix 10
-    // fix 2020
-    // confi 666
-    // conflict 999
-    // nico
-    // major
+    // feat #1
 }
